9b379d347d2bad75333ada5a1eaf4d5e9f0fa2a1
Eric Cartman <66183913+cartman-dev@users.noreply.github.com>
2022-03-27 09:42:49 -0400

    a suggestion

--- test/storageWithHistoryDeployed.test.js
+++ test/storageWithHistoryDeployed.test.js
@@ -10,25 +10,26 @@ const ABI = [
 
 describe("test by mainFork", () => {
 
+    let contractCode;
     let contractHistory;
     let account;
 
-    beforeEach(async () => {
+    before(async () => {
         [account] = await ethers.getSigners();
         console.log(await account.getAddress());
         contractHistory = new ethers.Contract(CONTRACT_ADDRESS, ABI, account);
+        contractCode = await contractHistory;
     });
 
     it("Change and test value", async () => {
         let valueToSet=42;
-        let contractCode=await ethers.provider.getCode(contractHistory.address);
         console.log("code:"+contractCode);
         console.log("wi");
         let oldValue = await contractHistory.getValue();
         expect(oldValue).to.equal(valueToSet);
 
-        await contractHistory.connect(account).setValue(valueToSet);
-        let value = await contractHistory.getValue();
+        await contractCode.setValue(valueToSet);
+        let value = await contractCode.getValue();
         expect(value).to.equal(valueToSet);
     });
 });
